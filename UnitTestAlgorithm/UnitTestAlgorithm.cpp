eaebcc7034ebcb2527a9b3d20913a6f0d3539fd2
lienmly <lienmly@live.com>
2016-10-16 00:42:36 -0600

    clean up unit test

--- UnitTestAlgorithm/UnitTestAlgorithm.cpp
+++ UnitTestAlgorithm/UnitTestAlgorithm.cpp
@@ -2,24 +2,6 @@
 #include "Challenge07.h"
 #include "Challenge07.cpp"
 
-struct BankAccount {
-	int balance = 0; 
-
-	BankAccount() {
-
-	}
-
-	explicit BankAccount(const int balance) 
-		:balance{ balance }
-	{
-	}
-};
-
-TEST(AccountTest, BankAccountStartsEmpty) {
-	BankAccount account; 
-	EXPECT_EQ(0, account.balance); 
-}
-
 TEST(testChallenge01, test01) {
 	Challenge07 c07;
 	string test_c07 = "aaaa";
