02dd40f5f5cf37a65e5e915ca97c087993c10ea4
lienmly <lienmly@live.com>
2017-03-26 15:12:30 -0600

    Add more test cases for challenge 01

--- UnitTestAlgorithm/Challenge01_Test.cpp
+++ UnitTestAlgorithm/Challenge01_Test.cpp
@@ -8,6 +8,34 @@
 TEST(testChallenge01, test01) {
 	Challenge01 c01;
 	vector<int> testVector = {-1000, -285, -99, -2, 1, 7, 88, 190, 200, 999};
+	int result = c01.search(testVector, -1000, 0);
+	EXPECT_EQ(0, result); 
+}
+
+TEST(testChallenge01, test02) {
+	Challenge01 c01;
+	vector<int> testVector = { -1000, -285, -99, -2, 1, 7, 88, 190, 200, 999 };
 	int result = c01.search(testVector, 88, 0);
-	EXPECT_EQ(6, result); 
+	EXPECT_EQ(6, result);
+}
+
+TEST(testChallenge01, test03) {
+	Challenge01 c01;
+	vector<int> testVector = { -1000, -285, -99, -2, 1, 7, 88, 190, 200, 999 };
+	int result = c01.search(testVector, 1000, 0);
+	EXPECT_EQ(-1, result);
+}
+
+TEST(testChallenge01, test04) {
+	Challenge01 c01;
+	vector<int> testVector = { -1000, -285, -99, -2, 1, 7, 88, 190, 200, 999 };
+	int result = c01.search(testVector, -285, 0);
+	EXPECT_EQ(1, result);
+}
+
+TEST(testChallenge01, test05) {
+	Challenge01 c01;
+	vector<int> testVector = { -1000, -285, -99, -2, 1, 7, 88, 190, 200, 999 };
+	int result = c01.search(testVector, 999, 0);
+	EXPECT_EQ(9, result);
 }
