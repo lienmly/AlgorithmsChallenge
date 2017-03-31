285776f2b3f1165cf6a9e43117c9f594ea4fef5b
lienmly <lienmly@live.com>
2017-03-30 21:16:19 -0600

    Add TODO for Challenge02 TEST + Add BinarySearchTree class

---
+++ Algorithms/Include/01-50/Challenge21.h
@@ -0,0 +1,29 @@
+//=====================================================================================
+// 
+// Lien Ly 
+// 
+//-------------------------------------------------------------------------------------
+// Description
+// Build binary search tree class from scratch 
+// 
+// Challenge21.h - Binary Search Tree
+//=====================================================================================
+#ifndef CHALLENGE21_H
+#define CHALLENGE21_H
+
+template <typename T> 
+struct TreeNode {
+	T value; 
+	TreeNode* left; 
+	TreeNode* right; 
+};
+
+template <typename T>
+class BinarySearchTree {
+public:
+	// Constructors
+
+private:
+};
+
+#endif
